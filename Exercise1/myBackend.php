96d66be6bee8664bae5bed22190c795ca82b3904
Joshua Berkley <j708b537@ku.edu>
2020-11-13 16:07:12 -0600

    Initial commit

---
+++ Exercise1/myBackend.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+echo "<table border=\"1\" style='border-collapse: collapse'>";
+for ($i=1; $i < 100; $i++)
+{
+  echo "<tr> \n";
+  for ($j=0; $j < ; $j++)
+  {
+    $k = $i * $j;
+    echo "<td>$k</td> \n";
+  }
+  echo "</tr>";
+}
+echo "</table>"
+
+ ?>
