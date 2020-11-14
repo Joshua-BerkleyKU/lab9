5cbf7bc4771cf07faceae65f1aaf179f984eaffe
Joshua Berkley <j708b537@ku.edu>
2020-11-14 15:24:18 -0600

    Update Quiz.php

--- Exercise2/Quiz.php
+++ Exercise2/Quiz.php
@@ -9,8 +9,9 @@
 
 
   echo "<span style='font-size:.8em;'>";
-/*
+
   echo "Question 1: what engineering major is the best<br />";
+  /*
   if ($question1 == "A: Computer Science")
   {
     $correctnum++;
