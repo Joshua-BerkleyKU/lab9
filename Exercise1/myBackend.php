c352d900471000340567bf78a78923bf502c01c4
Joshua Berkley <j708b537@ku.edu>
2020-11-13 22:04:18 -0600

    Update myBackend.php

--- Exercise1/myBackend.php
+++ Exercise1/myBackend.php
@@ -3,10 +3,10 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 echo "<table style='font-size:.5em;'>";
-for ($i=1; $i < 100; $i++)
+for ($i=1; $i < 101; $i++)
 {
   echo "<tr> \n";
-  for ($j=0; $j < 100 ; $j++)
+  for ($j=1; $j < 101 ; $j++)
   {
     $k = $i * $j;
     echo "<td>$k</td> \n";
