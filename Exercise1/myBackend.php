5bbd37ef8ef17eb5ff51c0ab7e666a4c71c4e266
Joshua Berkley <j708b537@ku.edu>
2020-11-13 22:00:07 -0600

    adding things

--- Exercise1/myBackend.php
+++ Exercise1/myBackend.php
@@ -6,7 +6,7 @@ echo "<table style='font-size:.5em;'>";
 for ($i=1; $i < 100; $i++)
 {
   echo "<tr> \n";
-  for ($j=0; $j < ; $j++)
+  for ($j=0; $j < 100 ; $j++)
   {
     $k = $i * $j;
     echo "<td>$k</td> \n";
