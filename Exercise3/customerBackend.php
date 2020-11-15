35bb5bd8c345802358a8c79c01619614e14d5d52
Joshua Berkley <j708b537@ku.edu>
2020-11-14 22:39:23 -0600

    Update customerBackend.php

--- Exercise3/customerBackend.php
+++ Exercise3/customerBackend.php
@@ -6,6 +6,11 @@
   $item2 = $_POST["Computerparts"];
   $item3 = $_POST["steam"];
 
+  $temp1 = $item1 * 20;
+  $temp2 = $item2 * 100;
+  $temp3 = $item3 * 10;
+  $temp4 = $temp1 + $temp2 + $temp3;
+
   echo "<link rel='stylesheet' type='text/css' href='style.css' />";
   echo "<p id='welcome'>";
     echo "Welcome $email <br />";
@@ -19,4 +24,53 @@
     echo "<th>Sub Total</th>";
   echo "</thead>";
   echo "<tbody>";
+    echo "<tr>";
+      echo "<td>minecraft</td>";
+      echo "<td> $item1 </td>";
+      echo "<td>$20</td>";
+      echo "<td>$$temp1</td>";
+    echo "</tr>";
+    echo "<tr>";
+      echo "<td>Computer parts</td>";
+      echo "<td> $item2 </td>";
+      echo "<td>$100</td>";
+      echo "<td>$$temp2</td>";
+    echo "</tr>";
+    echo "<tr>";
+      echo "<td>steam</td>";
+      echo "<td> $item3 </td>";
+      echo "<td>$10</td>";
+      echo "<td>$$temp3</td>";
+    echo "</tr>";
+    if ($shipping == "Free 7 day")
+    {
+      echo "<tr>";
+        echo "<td>$shipping</td>";
+        echo "<td> 1 </td>";
+        echo "<td>$0</td>";
+        echo "<td>$0</td>";
+      echo "</tr>";
+    }
+    if ($shipping == "$50.00 over night")
+    {
+      echo "<tr>";
+        echo "<td>$shipping</td>";
+        echo "<td> 1 </td>";
+        echo "<td>$50</td>";
+        echo "<td>$50</td>";
+      echo "</tr>";
+    }
+    if ($shipping == "$5.00 three day") {
+      echo "<tr>";
+        echo "<td>$shipping</td>";
+        echo "<td> 1 </td>";
+        echo "<td>$5</td>";
+        echo "<td>$5</td>";
+      echo "</tr>";
+    }
+    echo "<tr>";
+      echo "<td colspan='3'>Total:</td>";
+      echo "<td> $$temp4 </td>";
+    echo "</tr>";
+  echo "</tbody></table>";
 ?>
